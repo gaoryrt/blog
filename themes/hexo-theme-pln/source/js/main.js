12b1f6e01bed17c0aa015fa4fb255de0272c76bb
gaoryrt <gaoryrt@gmail.com>
2022-10-09 16:45:39 +0800

    remove: 🔥 remove log

--- themes/hexo-theme-pln/source/js/main.js
+++ themes/hexo-theme-pln/source/js/main.js
@@ -4,11 +4,11 @@ const registerServiceWorker = async () => {
     try {
       const registration = await navigator.serviceWorker.register("/service-worker.js");
       if (registration.installing) {
-        console.log("Service worker installing");
+        // console.log("Service worker installing");
       } else if (registration.waiting) {
-        console.log("Service worker installed");
+        // console.log("Service worker installed");
       } else if (registration.active) {
-        console.log("Service worker active");
+        // console.log("Service worker active");
       }
     } catch (error) {
       console.error(`Registration failed with ${error}`);
--- themes/hexo-theme-pln/source/service-worker.js
+++ themes/hexo-theme-pln/source/service-worker.js
@@ -1,11 +1,10 @@
 // Choose a cache name
-const cacheName = 'gaoryrt.com.1';
+const cacheName = 'gaoryrt.com.101';
 // List the files to precache
 const precacheResources = ['/', '/css/m.min.css', '/js/zoom-image.js'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
-  console.log('Service worker install event!', precacheResources);
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
 });
 
@@ -25,12 +24,9 @@ self.addEventListener('activate', (event) => {
 
 // When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
 self.addEventListener('fetch', (event) => {
-  console.log('Fetch intercepted for:', event.request.url);
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      console.log('matched')
       return cachedResponse || fetch(event.request).then(function(response) {
-        console.log(response)
         return caches.open(cacheName).then(function(cache) {
           cache.put(event.request, response.clone());
           return response;
