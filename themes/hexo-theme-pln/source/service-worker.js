85f5e4a2562aca57db1a1ef0447c1740c346c077
gaoryrt <gaoryrt@gmail.com>
2022-10-09 13:44:54 +0800

    new: ✨ service-worker

---
+++ themes/hexo-theme-pln/source/service-worker.js
@@ -0,0 +1,41 @@
+// Choose a cache name
+const cacheName = 'gaoryrt.com.1';
+// List the files to precache
+const precacheResources = ['/', '/css/m.min.css', '/js/zoom-image.js'];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!', precacheResources);
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+self.addEventListener('activate', (event) => {
+  var cacheWhitelist = [cacheName];
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      console.log('matched')
+      return cachedResponse || fetch(event.request).then(function(response) {
+        console.log(response)
+        return caches.open(cacheName).then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    }),
+  );
+});
